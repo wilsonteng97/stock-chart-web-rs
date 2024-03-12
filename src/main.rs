2c59f310fc60dd578b7deedece5e565afa7b1262
Cédric Meuter <cedric.meuter@gmail.com>
2024-03-12 09:22:19 +0100

    grabbing APPL stock price for date range

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use std::error::Error;
+use time::macros::datetime;
+use yahoo_finance_api as yahoo;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello, world!");
+    let provider = yahoo::YahooConnector::new();
+    let start = datetime!(2020-1-1 0:00:00.00 UTC);
+    let end = datetime!(2020-1-31 23:59:59.99 UTC);
+    let resp = provider.get_quote_history("AAPL", start, end).await?;
+    let quotes = resp.quotes()?;
+    println!("Apple's quotes in January 2020: {:?}", quotes);
+
     Ok(())
 }
