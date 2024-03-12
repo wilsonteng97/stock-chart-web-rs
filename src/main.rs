02f6638545f4e6e98ab3f0349baea2a272b9024d
Cédric Meuter <cedric.meuter@gmail.com>
2024-03-12 10:34:46 +0100

    quotes are now returned as Json values

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,36 @@
 use axum::{
-    debug_handler, extract::State, http::StatusCode, response::IntoResponse, routing::get, Router,
+    debug_handler, extract::State, http::StatusCode, response::IntoResponse, routing::get, Json,
+    Router,
 };
+use serde_json::json;
 use std::{error::Error, sync::Arc};
 use time::macros::datetime;
 use tokio::sync::Mutex;
-use yahoo_finance_api::{Quote, YahooConnector};
+use yahoo_finance_api::{Quote, YahooConnector, YahooError};
 
 type AppState = Arc<Mutex<YahooConnector>>;
 
-async fn get_quotes(yahoo: &YahooConnector) -> Result<Vec<Quote>, Box<dyn Error>> {
+async fn get_quotes(yahoo: &YahooConnector) -> Result<Vec<Quote>, YahooError> {
     let start = datetime!(2020-1-1 0:00:00.00 UTC);
     let end = datetime!(2020-1-31 23:59:59.99 UTC);
     let resp = yahoo.get_quote_history("AAPL", start, end).await?;
-    Ok(resp.quotes()?)
+    resp.quotes()
 }
 
 #[debug_handler]
 async fn landing_page_handler(State(state): State<AppState>) -> impl IntoResponse {
     let yahoo = state.lock().await;
     match get_quotes(&yahoo).await {
-        Ok(quotes) => (StatusCode::OK, format!("{:#?}", quotes)),
-        Err(error) => (StatusCode::INTERNAL_SERVER_ERROR, error.to_string()),
+        Ok(quotes) => {
+            let status = StatusCode::OK;
+            let payload = Json(quotes);
+            (status, payload).into_response()
+        }
+        Err(error) => {
+            let status = StatusCode::INTERNAL_SERVER_ERROR;
+            let payload = Json(json!({ "error": error.to_string() }));
+            (status, payload).into_response()
+        }
     }
 }
 
