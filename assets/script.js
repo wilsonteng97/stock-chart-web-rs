6219a5ef93f9a22aad634b374fd257cb3cf9f7f1
Cédric Meuter <cedric.meuter@gmail.com>
2024-03-12 15:44:56 +0100

    added the possibility to pick the range

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,39 @@
 (async () => {
 
-    // TODO: pick this from the list
-    const ticker = "NVDA";
-    const stock_name = "Nvidia Corp."
+    const url_params = new URLSearchParams(window.location.search);
+    url_params.set("ticker", url_params.get("ticker") || "AAPL")
+    url_params.set("interval", url_params.get("interval") || "1d")
+    url_params.set("period", url_params.get("period") || "1y")
 
     // TODO: currency should be returned in the quotes
     const currency = "$";
 
-    const response = await fetch("/api/v1/quotes?ticker=" + ticker);
+    const ticker = url_params.get("ticker").toUpperCase()
+    const response = await fetch("/api/v1/quotes?" + url_params.toString());
     const quotes = await response.json()
     const timestamps = quotes.map((quote) => new Date(quote.timestamp * 1000))
     const stock_prices = quotes.map((quote) => quote.close)
 
-    document.getElementById("ticker").textContent = stock_name
+    document.getElementById("ticker").textContent = ticker
+
+    const all_periods = [
+        "1mo",
+        "6mo",
+        "ytd",
+        "1y",
+        "5y"
+    ]
+
+    for (period of all_periods) {
+        const element = document.getElementById("period-" + period)
+        const parent = element.parentNode;
+        const link = document.createElement("a")
+        url_params.set("period", period)
+        link.href = "?" + url_params.toString()
+        link.appendChild(element.cloneNode(true))
+        parent.replaceChild(link, element)
+    }
+
 
     new Chart("chart", {
         type: "line",
